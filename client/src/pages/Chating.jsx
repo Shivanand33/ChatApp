dfa5abeec9fc53bd38f19bf6b2bae55d277fc62c
Shivanand33 <chaurasiyashivanand01@gmail.com>
2026-03-10 21:08:24 +0530

    chating

--- client/src/pages/Chating.jsx
+++ client/src/pages/Chating.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
 import QuickNavigation from "../components/chat/QuickNavigation";
+import ContactBar from "../components/chat/ContactBar";
 import ChatWindow from "../components/chat/ChatWindow";
-import ContactBar from "../components/chat/Contactbar";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import socketAPI from "../config/WebSocket";
+
 const Chating = () => {
-  const [fetchMode, setFetchMode] = useState("RC");
+  const { user } = useAuth();
+  const [fetchMode, setFetchMode] = useState("AC");
 
   const [receiver, setReceiver] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      socketAPI.emit("createPath", user._id);
+    }
+
+    return () => {
+      socketAPI.emit("destroyPath", user._id);
+    };
+  }, []);
 
   return (
     <>
       <div className="flex h-[92vh]">
         <div className="w-1/20 border-r-2 border-gray-300 overflow-hidden">
-          <QuickNavigation setFetchMode={setFetchMode} />
+          <QuickNavigation setFetchMode={setFetchMode} fetchMode={fetchMode} />
         </div>
         <div className="w-4/20 border-r-2 border-gray-300 overflow-hidden">
           <ContactBar fetchMode={fetchMode} setReceiver={setReceiver} />
